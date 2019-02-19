aebca027c681c0ab624df23e3f09c2bcee5cfed3
Jane Venter <janeanneventer@gmail.com>
2019-02-19 00:35:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,16 +4,16 @@ class App < Sinatra::Base
   get '/' do 
     "Hello World"
   end
-  get '/name' do 
+  get '/name/' do 
     "My name is "
   end 
   
-  get '/hometown' do 
+  get '/hometown/' do 
     "My hometown is "
   end 
   
   
-  get '/favorite-song' do
+  get '/favorite-song/' do
     "My favorite song is "
   end
   
